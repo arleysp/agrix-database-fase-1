b878120d50368403a7ae1ff7aaa5c10fdf9ec22f
Arley Pereira <arleyspereira@hotmail.com>
2024-07-22 20:45:02 -0300

    Feat: Requisito 7 - Rota Get Crop By Id

--- src/main/java/com/betrybe/agrix/controller/CropController.java
+++ src/main/java/com/betrybe/agrix/controller/CropController.java
@@ -2,9 +2,11 @@ package com.betrybe.agrix.controller;
 
 import com.betrybe.agrix.controller.dto.CropDto;
 import com.betrybe.agrix.service.CropService;
+import com.betrybe.agrix.service.exception.CropNotFoundException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,5 +40,17 @@ public class CropController {
         .stream()
         .map(CropDto::fromEntity)
         .toList();
+  }
+
+  /**
+   * Gets crop by id.
+   *
+   * @param id the id
+   * @return the crop by id
+   * @throws CropNotFoundException the crop not found exception
+   */
+  @GetMapping("/{id}")
+  public CropDto getCropById(@PathVariable Long id) throws CropNotFoundException {
+    return CropDto.fromEntity(cropService.findById(id));
   }
 }
--- src/main/java/com/betrybe/agrix/service/CropService.java
+++ src/main/java/com/betrybe/agrix/service/CropService.java
@@ -2,6 +2,7 @@ package com.betrybe.agrix.service;
 
 import com.betrybe.agrix.entity.Crop;
 import com.betrybe.agrix.repository.CropRepository;
+import com.betrybe.agrix.service.exception.CropNotFoundException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,18 @@ public class CropService {
    */
   public List<Crop> findAll() {
     return cropRepository.findAll();
+  }
+
+  /**
+   * Find by id crop.
+   *
+   * @param id the id
+   * @return the crop
+   * @throws CropNotFoundException the crop not found exception
+   */
+  public Crop findById(Long id) throws CropNotFoundException {
+    return cropRepository.findById(id)
+        .orElseThrow(CropNotFoundException::new);
   }
 
 }
